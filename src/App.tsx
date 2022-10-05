1f587bc170c920cb97c4d023580e2d9d39d8978e
Lucas Rosa <lucasmetron@gmail.com>
2022-10-05 09:50:18 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Teste1 from './components/Teste1';
 import Teste2 from './components/teste2';
+import Teste3 from './components/Teste3';
 
 function App() {
 	return (
 		<div>
 			<Teste1 />
 			<Teste2 />
-			sddsd asas
+			<Teste3 />
 		</div>
 	);
 }
---
+++ src/components/Teste3/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Teste3() {
+	return (
+		<div>
+			<p>Lucas Rosa</p>
+			<h1>teste</h1>
+		</div>
+	);
+}
